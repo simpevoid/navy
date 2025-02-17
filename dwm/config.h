ac472b2e86f56a8fb1af2157f6d9d356f0362292
Your Name <fellowquniq@gmail.com>
2025-02-17 22:17:23 +0200

    new updates

--- dwm/config.h
+++ dwm/config.h
@@ -13,7 +13,7 @@ static const char col_gray4[] = "#49688e";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray1 },
-    [SchemeSel] = {col_gray2, col_gray4, col_gray2 },
+    [SchemeSel] = {col_gray2, col_gray3, col_gray2 },
 };
 
 /* tagging */
